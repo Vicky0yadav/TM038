b33199db90fa1da529fe431ae96060876abec52e
Vicky Yadav <113773168+Vicky0yadav@users.noreply.github.com>
2025-01-09 21:29:35 +0530

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,9 @@
-function view(){
-    alert("Please first sign up to show Portfolio");
+function view() {
+    alert("Please sign up first to view the portfolio.");
+    const userResponse = confirm("Would you like to sign up now?");
+    if (userResponse) {
+        window.location.href = "login.php"; 
+    } else {
+        alert("You can sign up anytime to access the portfolio.");
+    }
 }
